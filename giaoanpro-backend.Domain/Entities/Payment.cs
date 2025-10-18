eaaec1b3af1242759866469de246adf488143564
Phuocws <nhp309@gmail.com>
2025-10-18 16:59:09 +0700

    feat: subscription plan crud. refactor: application denpendency inject, add payment entity, refactor relative entities.

---
+++ giaoanpro-backend.Domain/Entities/Payment.cs
@@ -0,0 +1,22 @@
+﻿using giaoanpro_backend.Domain.Bases;
+using giaoanpro_backend.Domain.Enums;
+
+namespace giaoanpro_backend.Domain.Entities
+{
+	public class Payment : AuditableEntity, ISoftDeleteEntity
+	{
+		public Guid Id { get; set; }
+
+		public Guid SubscriptionId { get; set; }
+
+		public decimal AmountPaid { get; set; }
+		public DateTime PaymentDate { get; set; }
+		public PaymentStatus Status { get; set; }
+		public string PaymentMethod { get; set; } = string.Empty; // "VNPAY", "Stripe", "MoMo"
+		public string GatewayTransactionId { get; set; } = string.Empty;
+
+		// Navigation property
+		public virtual Subscription Subscription { get; set; } = null!;
+		public DateTime? DeletedAt { get; set; }
+	}
+}
---
+++ giaoanpro-backend.Persistence/Migrations/20251018111828_AddIsActivityIntoSubscriptionPlan.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace giaoanpro_backend.Persistence.Migrations
+{
+	/// <inheritdoc />
+	public partial class AddIsActivityIntoSubscriptionPlan : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.AddColumn<bool>(
+				name: "IsActive",
+				table: "SubscriptionPlans",
+				type: "bit",
+				nullable: false,
+				defaultValue: false);
+
+			migrationBuilder.CreateTable(
+				name: "Payments",
+				columns: table => new
+				{
+					Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+					SubscriptionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+					AmountPaid = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+					PaymentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+					Status = table.Column<int>(type: "int", nullable: false),
+					PaymentMethod = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					GatewayTransactionId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+					CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+					UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+				},
+				constraints: table =>
+				{
+					table.PrimaryKey("PK_Payments", x => x.Id);
+					table.ForeignKey(
+						name: "FK_Payments_Subscriptions_SubscriptionId",
+						column: x => x.SubscriptionId,
+						principalTable: "Subscriptions",
+						principalColumn: "Id",
+						onDelete: ReferentialAction.Cascade);
+				});
+
+			migrationBuilder.CreateIndex(
+				name: "IX_Payments_SubscriptionId",
+				table: "Payments",
+				column: "SubscriptionId");
+		}
+
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropTable(
+				name: "Payments");
+
+			migrationBuilder.DropColumn(
+				name: "IsActive",
+				table: "SubscriptionPlans");
+		}
+	}
+}
