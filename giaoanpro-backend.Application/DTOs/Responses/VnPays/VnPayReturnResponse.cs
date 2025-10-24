e623df7e9e534eefeb3c85d84924daaa08abd4f9
Phuocws <nhp309@gmail.com>
2025-10-24 19:36:02 +0700

    refactor: handle callback in ipn url, return id in return url only.

---
+++ giaoanpro-backend.Application/DTOs/Responses/VnPays/VnPayReturnResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace giaoanpro_backend.Application.DTOs.Responses.VnPays
+{
+	public class VnPayReturnResponse
+	{
+		public Guid PaymentId { get; set; }
+		public Guid SubscriptionId { get; set; }
+	}
+}
