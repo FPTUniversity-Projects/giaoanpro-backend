cda9eeb25dc6482a8bf201e9956265d5bd89eb51
Phuocws <nhp309@gmail.com>
2025-11-17 09:45:43 +0700

    feat: add subscription plans lookup endpoints for user and admin.

--- giaoanpro-backend.Application/Interfaces/Services/ISubscriptionPlanService.cs
+++ giaoanpro-backend.Application/Interfaces/Services/ISubscriptionPlanService.cs
@@ -8,6 +8,8 @@ namespace giaoanpro_backend.Application.Interfaces.Services
 	{
 		public Task<BaseResponse<PagedResult<GetSubscriptionPlanResponse>>> GetSubscriptionPlansAsync(GetSubscriptionPlansQuery query);
 		public Task<BaseResponse<GetSubscriptionPlanResponse>> GetSubscriptionPlanByIdAsync(Guid id);
+		public Task<BaseResponse<List<SubscriptionPlanLookupResponse>>> GetSubscriptionPlanLookupsAsync();
+		public Task<BaseResponse<List<SubscriptionPlanLookupResponse>>> GetSubscriptionPlanAdminLookupsAsync(bool isActiveOnly);
 		public Task<BaseResponse<string>> CreateSubscriptionPlanAsync(CreateSubscriptionPlanRequest request);
 		public Task<BaseResponse<string>> UpdateSubscriptionPlanAsync(Guid id, UpdateSubscriptionPlanRequest request);
 		public Task<BaseResponse<string>> DeleteSubscriptionPlanAsync(Guid id);
