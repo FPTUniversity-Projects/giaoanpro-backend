c0bfb9dde662e2f55339586b4af348193efc24b8
Phuocws <nhp309@gmail.com>
2025-11-17 20:09:47 +0700

    feat: add user lookup endpoints

---
+++ giaoanpro-backend.Application/DTOs/Requests/Users/GetUserLookupRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace giaoanpro_backend.Application.DTOs.Requests.Users
+{
+	public class GetUserLookupRequest
+	{
+		public bool IncludeInactive { get; set; } = false;
+		public bool IncludeAdmins { get; set; } = false;
+	}
+}
---
+++ giaoanpro-backend.Application/Mappings/UserRegister.cs
@@ -0,0 +1,18 @@
+﻿using giaoanpro_backend.Application.DTOs.Responses.Users;
+using giaoanpro_backend.Domain.Entities;
+using Mapster;
+
+namespace giaoanpro_backend.Application.Mappings
+{
+	public class UserRegister : IRegister
+	{
+		public void Register(TypeAdapterConfig config)
+		{
+			config.NewConfig<User, GetUserLookupResponse>()
+				.Map(dest => dest.Id, src => src.Id)
+				.Map(dest => dest.Email, src => src.Email)
+				.Map(dest => dest.FullName, src => src.FullName)
+				.Map(dest => dest.Role, src => src.Role);
+		}
+	}
+}
