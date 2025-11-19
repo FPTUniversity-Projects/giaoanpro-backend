492df8289575384b73f7e04a2ba7ee0c48541b4e
Phuocws <nhp309@gmail.com>
2025-11-19 11:46:34 +0700

    feat: user management for user and admin.

---
+++ giaoanpro-backend.Application/DTOs/Requests/Users/UpdateCurrentUserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace giaoanpro_backend.Application.DTOs.Requests.Users
+{
+	public class UpdateCurrentUserRequest
+	{
+		public string? FullName { get; set; }
+		public string? Username { get; set; }
+	}
+}
